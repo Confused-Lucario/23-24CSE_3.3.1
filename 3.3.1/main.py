8460bcef5d9dd4f4c25ce5cccbb16263de03a7a9
The Cat <philip.birkland@vtsd.com>
2024-04-30 13:54:04 -0400

    Philip Brandon 3.3.1

---
+++ 3.3.1/main.py
@@ -0,0 +1,46 @@
+# Philip Birkland, Brandon Howland Calculator
+# These are the functions of a calculator
+
+import math
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def power(x,y):
+    return x^y
+
+def square_root(x):
+    return math.sqrt(x)
+
+def sin(x):
+    return math.sin(x)
+
+def cos(x):
+    return math.cos(x)
+
+def tan(x):
+    return math.tan(x)
+
+print("what would you like the calculator to do?" + "\n 1. add" + "\n 2. subtract" + "\n 3. multiply" "\n 4. divide" + "\n 5. power" + "\n 6. square root" + "\n 7. Sin" + "\n 8. cos" + "\n 9. tan")
+
+while True:
+    choice = int(input("choose 1, 2, 3, 4, 5 ,6 ,7, 8, 9"))
+    if choice == (1, 2, 3, 4, 5):
+        num1 = int(input("enter first number"))
+        num2 = int(input("enter second number"))
+        if
+
+    elif choice == (6, 7, 8, 9):
+
+    else:
+
+        
